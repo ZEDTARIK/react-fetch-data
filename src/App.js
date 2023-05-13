69a2a422d2aeb0d9f907ec44e44540652584583d
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-05-14 00:25:37 +0100

    Fetch data using simple case

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  //const [coolumn, setColumn] = useState();
+  const [records, setRecord] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setRecord(data)
+      })
+      .catch(err => console.error(err))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row m-3 p-3'>
+        <div className='col-sm-10 mx-auto'>
+          <div className='card'>
+            <div className='card-header text-center'>Fetch Data</div>
+            <div className='card-body'>
+              <table className='table table-hover'>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {
+                    records.map((record, index) =>
+                    (
+                      <tr key={index}>
+                        <td>{record.name}</td>
+                        <td>{record.email}</td>
+                        <td>{record.address.city}</td>
+                        <td>{record.phone}</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
